8b1f570b5e378aaac15189c22e65fa98eaf60ac0
maxgurewitz <maxthegeek1@gmail.com>
2015-11-07 00:35:53 -0800

    remove lodash as a dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 global.__BASE = __dirname;
 
 var gulp = require('gulp');
-var _ = require('lodash');
+var R = require('ramda');
 var taskFiles = require('./gulp/tasks');
 
-_.each(taskFiles, function(taskFile, taskFileName) {
-  _.each(taskFile, function(taskValues, taskKey) {
+R.mapObjIndexed(function(taskFile, taskFileName) {
+  R.mapObjIndexed(function(taskValues, taskKey) {
     var taskName = taskFileName + ':' + taskKey;
     var dependencies = taskValues.dependencies || [];
 
     gulp.task(taskName, dependencies, function(cb) { return taskValues.task(cb, gulp); });
-  });
-});
+  }, taskFile);
+}, taskFiles);
 
 gulp.task('default', ['bundle:watch', 'connect:start']);
