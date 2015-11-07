5843467f1faec58912663aff29c08b8d27f7c81e
Max Gurewitz <maxthegeek1@gmail.com>
2015-11-06 20:08:33 -0800

    chai things

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,10 @@
 var chai = require('chai');
 var joi = require('joi');
 var chaiJoi = require('chai-joi');
+var chaiThings = require('chai-things');
 
 chai.use(chaiJoi);
+chai.use(chaiThings);
 
 module.exports = {
   chai: chai,
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,7 +1,6 @@
 var utils = require(__BASE + '/src/utils');
 var helper = require('./helper');
 var settings = require(__BASE + '/src/settings');
-var R = require('ramda');
 
 var expect = helper.expect;
 var joi = helper.joi;
@@ -16,35 +15,45 @@ describe('utils', function() {
       }))
     });
 
-    var currentDay = 5;
-    var december2015 = utils.generateMonthFactory(currentDay, 11, 2015)(11, 2015);
+    describe('december 2015', function() {
+      var currentDay = 5;
+      var december2015 = utils.generateMonthFactory(currentDay, 11, 2015)(11, 2015);
 
-    var isDisabled = december2015.displayedDays.map(function(day) {
-      return day.isDisabled;
+      var isDisabled = december2015.displayedDays.map(function(day) {
+        return day.isDisabled;
+      });
+
+      it('generates correctly formatted output', function() {
+        expect(joi.validate(december2015, monthSchema)).to.validate;
+      });
+
+      it('the calendar displays the right number of days', function() {
+        expect(december2015.displayedDays).to.have.length(42);
+      });
+
+      it('disables days from the previous month', function() {
+        expect(isDisabled.slice(0, 2)).to.all.equal(true);
+      });
+
+      it('disables days from december before the current date', function() {
+        expect(isDisabled.slice(2, currentDay + 1)).to.all.equal(true);
+      });
+
+      it('enables days from december on or after the current date', function() {
+        expect(isDisabled.slice(currentDay + 2,  33)).to.all.equal(false);
+      });
+
+      it('disables days from the next month', function() {
+        expect(isDisabled.slice(33, 42)).to.all.equal(true);
+      });
     });
 
-    it('generates correctly formatted output', function() {
-      expect(joi.validate(december2015, monthSchema)).to.validate;
-    });
+    describe('november 2015', function() {
+      var november2015 = utils.generateMonthFactory(6, 10, 2015)(10, 2015);
 
-    it('the calendar displays the right number of days', function() {
-      expect(december2015.displayedDays).to.have.length(42);
-    });
-
-    it('disables days from the previous month', function() {
-      expect(R.all(R.identity, isDisabled.slice(0, 2))).to.be.true;
-    });
-
-    it('disables days from december before the current date', function() {
-      expect(R.all(R.identity, isDisabled.slice(2, currentDay + 1))).to.be.true;
-    });
-
-    it('enables days from december on or after the current date', function() {
-      expect(R.all(R.not, isDisabled.slice(currentDay + 2,  33))).to.be.true;
-    });
-
-    it('disables days from the next month', function() {
-      expect(R.all(R.identity, isDisabled.slice(33, 42))).to.be.true;
+      it('starts with november first', function() {
+        expect(november2015.displayedDays[0].dayOfMonth).to.equal(1);
+      });
     });
   });
 });
